128fa4240a02e32528c283d31aa4da51d22ed978
X19053 <>
2020-04-09 03:17:37 +0900

    入力フォームをGOで読み取れるようにしたゾ /addどっかやっちゃったけど

--- docker/go/app/main.go
+++ docker/go/app/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"text/template"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -34,15 +35,19 @@ func main() {
 	}
 	defer rows.Close()
 
-	fs := http.FileServer(http.Dir("html"))
-	http.Handle("/", fs)
-	http.HandleFunc("/add", addTask)
+	// fs := http.FileServer(http.Dir("html"))
+	// http.Handle("/", fs)
+	http.HandleFunc("/", topPage)
 	log.Println("Listening...")
 	http.ListenAndServe(":80", nil)
 }
 
-func addTask(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-
-	fmt.Println(r.Form["date"])
+func topPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method)
+	if r.Method == "POST" {
+		r.ParseForm()
+		fmt.Println(r.Form)
+	}
+	t, _ := template.ParseFiles("html/index.html")
+	t.Execute(w, nil)
 }
