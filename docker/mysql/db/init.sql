78b8d2b01af04aed25ca64e3421f62aeb73a1483
Kousuke Shibahara <kousukekappappa.210@outlook.jp>
2020-04-10 18:23:21 +0900

    idの重複を許しません

--- docker/mysql/db/init.sql
+++ docker/mysql/db/init.sql
@@ -1,18 +1,18 @@
 USE sample;
 
-CREATE TABLE tasks(
+CREATE TABLE  tasks(
     id INT auto_increment not null,
     datetime datetime,
     person_id int,
     contents varchar(4096),
-    index(id)
+    primary key(id)
 );
 
 create table persons(
-    id int auto_increment,
+    id int auto_increment not null,
     number varchar(128),
     name varchar(128),
-    index(id)
+    primary key(id)
 );
 
 insert into persons(number,name) values ("e19070","Shibahara"),("k19092","Fukuda"),("x19012","Inagaki"),("x19053","Suzuki");
