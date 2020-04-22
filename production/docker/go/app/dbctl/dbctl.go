77a88d87a90a773731d88a299fd9ff3ef4ff7873
HennecKabanchan <kappappa.sk1117210@gmail.com>
2020-04-23 02:31:05 +0900

    CallAddress関数を追加 使用していない構造体をコメントアウト

--- production/docker/go/app/dbctl/dbctl.go
+++ production/docker/go/app/dbctl/dbctl.go
@@ -8,49 +8,51 @@ import (
 	// _ "github.com/go-sql-driver/mysql"
 )
 
-// Place はデータベースのテーブルから値を取得するための構造体
-type Place struct {
-	PlaceID   int
-	PlaceName string
-	Name      string
-}
+// tableたちは使う時までコメントアウトしておく
+// // Place はデータベースのテーブルから値を取得するための構造体
+// type Place struct {
+// 	PlaceID   int
+// 	PlaceName string
+// 	Name      string
+// }
 
-// Book はデータベースのテーブルから値を取得するための構造体
-type Book struct {
-	RFID         string
-	BookName     string
-	Isbn         string
-	PlaceID      string
-	BookDatetime string
-}
+// // Book はデータベースのテーブルから値を取得するための構造体
+// type Book struct {
+// 	RFID         string
+// 	BookName     string
+// 	Isbn         string
+// 	PlaceID      string
+// 	BookDatetime string
+// }
 
-// Person はデータベースのテーブルから値を取得するための構造体
-type Person struct {
-	PersonID       int
-	CardData       string
-	PersonName     string
-	PersonEmail    string
-	Password       string
-	PersonDatetime string
-}
+// // Person はデータベースのテーブルから値を取得するための構造体
+// type Person struct {
+// 	PersonID       int
+// 	CardData       string
+// 	PersonName     string
+// 	PersonEmail    string
+// 	Password       string
+// 	PersonDatetime string
+// }
 
-// BorrowedLog はデータベースのテーブルから値を取得するための構造体
-type BorrowedLog struct {
-	BorrowedLogID int
-	RfidTag       string
-	PersonID      int
-}
+// // BorrowedLog はデータベースのテーブルから値を取得するための構造体
+// type BorrowedLog struct {
+// 	BorrowedLogID int
+// 	RfidTag       string
+// 	PersonID      int
+// }
 
-// PrePerson はデータベースのテーブルから値を取得するための構造体
-type PrePerson struct {
-	PrePersonID       int
-	PrePersonEmail    string
-	PrePersonToken    string
-	PrePersonDatetime string
-}
+// // PrePerson はデータベースのテーブルから値を取得するための構造体
+// type PrePerson struct {
+// 	PrePersonID       int
+// 	PrePersonEmail    string
+// 	PrePersonToken    string
+// 	PrePersonDatetime string
+// }
 
-// PreRegister は仮登録データベースにメールアドレスとそのトークンと時刻を登録する関数
+// PreRegister は仮登録データベースにメールアドレスとそのトークンと時刻を挿入する関数
 func PreRegister(mail, token string) {
+	//dbはめったに閉じる必要がないらしいがここでopenするのが適切かわからない
 	db, err := sql.Open("mysql", "gopher:setsetset@tcp(mysql:3306)/sample")
 	if err != nil {
 		// log.Println(err.Error())
@@ -69,4 +71,26 @@ func PreRegister(mail, token string) {
 	defer ins.Close()
 
 	ins.Exec(mail, token, time.Now().Format("2006-01-02 15:04:05"))
+}
+
+// CallAddress はメールアドレスを呼び出す関数
+func CallAddress(token string) (address string) {
+	db, err := sql.Open("mysql", "gopher:setsetset@tcp(mysql:3306)/sample")
+	if err != nil {
+		// log.Println(err.Error())
+		log.Println(err)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	//pre_person_tokenとtokenが一致するpre_person_emailをrowsに格納する
+	row, err := db.Query("select pre_person_email from pre_persons where pre_person_token like '%?%';", token)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer row.Close()
+
+	row.Scan(&address)
+
+	return address
 }
