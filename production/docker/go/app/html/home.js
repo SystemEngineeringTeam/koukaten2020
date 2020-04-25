29174e5d68c6b930f6096717195b63d98bf79274
x19012xx <x19012xx@aitech.ac.jp>
2020-04-25 20:10:14 +0900

    ホーム（仮）

---
+++ production/docker/go/app/html/home.js
@@ -0,0 +1,6 @@
+const rental = document.getElementById("rental");
+const existence = document.getElementById("existence");
+try {
+  rental.innerHTML = "貸";
+  existence.innerHTML = "在";
+} catch (e) {}
