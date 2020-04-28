99d57c826335fbb40fba6fb517c16cbe24156e22
x19012xx <x19012xx@aitech.ac.jp>
2020-04-29 02:17:55 +0900

    状態直し

--- production/docker/go/app/html/home.js
+++ production/docker/go/app/html/home.js
@@ -1,6 +1,7 @@
 const rental = document.getElementById("rental");
 const existence = document.getElementById("existence");
 try {
-  rental.innerHTML = "貸";
-  existence.innerHTML = "在";
+  Borrow.innerHTML = "貸";
+  Exist.innerHTML = "在";
+  TakeOut.innerHTML = "持"
 } catch (e) {}
