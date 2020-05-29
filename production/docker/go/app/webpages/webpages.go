b17e04bdc97534f04af18281fe9bc61d11936536
HennecKabanchan <kappappa.sk1117210@gmail.com>
2020-05-30 01:12:35 +0900

    本の詳細を表示するためのBookDetail関数を追加 BookStatus関数の引数をなしに変更 それに付随してBookStatus関数の不要な部分を削除

--- production/docker/go/app/webpages/webpages.go
+++ production/docker/go/app/webpages/webpages.go
@@ -23,7 +23,7 @@ func TopPage(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/login", http.StatusMovedPermanently)
 	}
 
-	books, err := dbctl.BookStatus(1)
+	books, err := dbctl.BookStatus()
 	if err != nil {
 		log.Println(err)
 		return
@@ -309,6 +309,5 @@ func UserPage(w http.ResponseWriter, r *http.Request) {
 
 //Test は新しく作った関数をテストするところ 関数の使い方も兼ねている
 func Test(w http.ResponseWriter, r *http.Request) {
-
-	// log.Println(dbctl.Login("e19070ee@aitech.ac.jp", "4c716d4cf211c7b7d2f3233c941771ad0507ea5bacf93b492766aa41ae9f720d"))
+	// log.Println(dbctl.BookDetail("apiのidを入れてね"))
 }
